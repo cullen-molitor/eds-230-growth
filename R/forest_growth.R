951da449eddd2dfb1b7cab176cfbb7f96e240b72
cullen-molitor <cullen.molitor@gmail.com>
2022-05-14 11:03:17 -0700

    Corrected function and plot

--- R/forest_growth.R
+++ R/forest_growth.R
@@ -1,10 +1,9 @@
 #' forest_growth
 #'
 #' @param time
-#' @param population
+#' @param forest_size C
 #' @param params$exp_growth_rate r
 #' @param params$lin_growth_rate g
-#' @param params$forest_size C
 #' @param params$carrying_capacity K
 #' @param params$canopy_closure_threshold 
 #'
@@ -12,12 +11,12 @@
 #'   
 forest_growth <- function(
     time,
-    population,
+    forest_size,
     params
 ){
   dC_dt = case_when(
-    params$forest_size < params$canopy_closure_threshold ~ params$exp_growth_rate * params$forest_size,
-    T ~ params$lin_growth_rate * (1 - (params$forest_size / params$carrying_capacity))
+    forest_size < params$canopy_closure_threshold ~ params$exp_growth_rate * forest_size,
+    T ~ params$lin_growth_rate * (1 - (forest_size / params$carrying_capacity))
   )
   return(list(dC_dt))
 }
