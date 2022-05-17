35344741464c610e36a524fecdd5b13c20b3c147
cullen-molitor <cullen.molitor@gmail.com>
2022-05-17 10:05:32 -0700

    Small updates to function and last chunk

--- R/forest_growth.R
+++ R/forest_growth.R
@@ -1,20 +1,20 @@
 #' forest_growth
 #'
-#' @param time
-#' @param forest_size C
-#' @param params$exp_growth_rate r
-#' @param params$lin_growth_rate g
-#' @param params$carrying_capacity K
-#' @param params$canopy_closure_threshold 
+#' @param time (Numeric) The number of years to run the model
+#' @param forest_size (Numeric) forest size as measured by carbon (C) in kilograms
+#' @param params$exp_growth_rate (Numeric) The exponential growth rate (r) that happens prior to reaching the canopy closure threshold
+#' @param params$lin_growth_rate (Numeric) The linear growth rate (g) that happens after a forest reaches the canopy closure threshold
+#' @param params$carrying_capacity (Numeric) The forest's carrying capacity (K) measured in kilograms
+#' @param params$canopy_closure_threshold (Numeric) The forests canopy closure threshold measured in kilograms
 #'
-#' @return dC_dt
+#' @return dC_dt the change in forest size (kg) over time (t)
 #'   
 forest_growth <- function(
     time,
     forest_size,
     params
 ){
-  dC_dt = case_when(
+  dC_dt <- case_when(
     forest_size < params$canopy_closure_threshold ~ params$exp_growth_rate * forest_size,
     T ~ params$lin_growth_rate * (1 - (forest_size / params$carrying_capacity))
   )
